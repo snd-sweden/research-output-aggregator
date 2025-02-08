bd3056f9187766003e2a9ff442cca646122a04ff
Olof Olsson <olof.olsson@snd.gu.se>
2025-02-08 15:35:20 +0100

    datacite api fixes
    creator/contributor organization should also match on ror on nameIdentifier

--- src/roagg/datacite.py
+++ src/roagg/datacite.py
@@ -22,28 +22,50 @@ def create_datacite_query_string(name: List[str] = [], ror: str = "") -> str:
         ror_fields = [
             "publisher.publisherIdentifier",
             "creators.affiliation.affiliationIdentifier", 
-            "contributors.affiliation.affiliationIdentifier"
+            "contributors.affiliation.affiliationIdentifier",
+            "creators.nameIdentifiers.nameIdentifier",
+            "contributors.nameIdentifiers.nameIdentifier"
         ]
 
         query_parts.extend([f'{field}:"{ror}"' for field in ror_fields])
     
     return " OR ".join(query_parts)
 
+def api_request_url(query: str, page_size: int = 100) -> str:    
+    params = urllib.parse.urlencode({
+        'page[size]': page_size,
+        'affiliation': 'true',
+        'publisher': 'true',
+        'query': query
+    })
+    return f"https://api.datacite.org/dois?{params}"
+
+def get_api_result(url: str) -> dict:
+    try:
+        with urllib.request.urlopen(url) as response:
+            return json.loads(response.read())
+      
+    except (urllib.error.URLError, json.JSONDecodeError, KeyError) as e:
+        raise RuntimeError(f"Failed run DataCite query: {e}")
+    
+def api_all(query: str) -> list:
+    result = []
+    page_size = 500
+    url = api_request_url(query, page_size)
+    while True:
+        response = get_api_result(url)
+        result.extend(response["data"])
+        print(f"Retrieved {len(result)} DOIs")
+
+        if response['links'].get('next'):
+            url = response['links']['next']
+        else:
+            break
+    
+    return result
+
 def datacite_query_result_count(query: str) -> int:
-    """Get total number of results for a DataCite query."""
     if not query:
         return 0
-        
-    try:
-        params = urllib.parse.urlencode({
-            'page[size]': 0,
-            'query': query
-        })
-        url = f"https://api.datacite.org/dois?{params}"
-        
-        with urllib.request.urlopen(url) as response:
-            data = json.loads(response.read())
-            return data["meta"]["total"]
-            
-    except (urllib.error.URLError, json.JSONDecodeError, KeyError) as e:
-        raise RuntimeError(f"Failed to get result count from DataCite: {e}")
+    url = api_request_url(query, 0)
+    return get_api_result(url)["meta"]["total"]
