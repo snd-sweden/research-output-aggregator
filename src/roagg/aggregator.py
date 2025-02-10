9ae9793659bcd5e2bf1d797974d0c1baf3d6255d
Olof Olsson <olof.olsson@snd.gu.se>
2025-02-10 10:17:15 +0100

    wip: refactor DataCite API into a class

--- src/roagg/aggregator.py
+++ src/roagg/aggregator.py
@@ -1,10 +1,10 @@
 from typing import List
 from roagg.ror import get_names_from_ror
-from roagg.datacite import create_datacite_query_string, datacite_query_result_count
+from roagg.datacite import DataCiteAPI
 import logging
+import json
 
 def aggregate(name: List[str] = [], ror: str = "") -> None:
-    # if ror is provided, get the name from the ror api
     if ror:
         ror_name = get_names_from_ror(ror)
         name.extend(ror_name)
@@ -13,8 +13,9 @@ def aggregate(name: List[str] = [], ror: str = "") -> None:
     name = list(set(name))
 
     # debug print of the query string
-    query = create_datacite_query_string(name, ror)
-    query_result_count = datacite_query_result_count(query)
-    logging.info("DataCite query:")
-    logging.info(query)
-    logging.info(f"Result count: {query_result_count}")
+    datacite = DataCiteAPI(name=name, ror=ror)
+    url = datacite.api_request_url()
+    count = datacite.count()
+    logging.info("DataCite url:")
+    logging.info(url)
+    logging.info(f"Result count: {count}")
--- src/roagg/datacite.py
+++ src/roagg/datacite.py
@@ -3,71 +3,77 @@ import urllib.request
 import logging
 import json
 
-def create_datacite_query_string(name: List[str] = [], ror: str = "") -> str:
-    if not name and not ror:
-        return ""
+class DataCiteAPI:
+    def __init__(self, page_size: int = 500, name: List[str] = [], ror: str = ""):
+        self.page_size = page_size
+        self.name = name
+        self.ror = ror
+
+    def get_query_string(self) -> str:
+        if not self.name and not self.ror:
+            return ""
+            
+        query_parts = []
         
-    query_parts = []
-    
-    if name:
-        name_conditions = ' OR '.join(f'"{n}"' for n in name)
-        name_fields = [
-            "creators.affiliation.name",
-            "contributors.affiliation.name", 
-            "publisher.name",
-            "creators.name",
-            "contributors.name"
-        ]
+        if self.name:
+            name_conditions = ' OR '.join(f'"{n}"' for n in self.name)
+            name_fields = [
+                "creators.affiliation.name",
+                "contributors.affiliation.name", 
+                "publisher.name",
+                "creators.name",
+                "contributors.name"
+            ]
+            
+            query_parts.extend([f"{field}:({name_conditions})" for field in name_fields])
         
-        query_parts.extend([f"{field}:({name_conditions})" for field in name_fields])
-    
-    if ror:
-        ror_fields = [
-            "publisher.publisherIdentifier",
-            "creators.affiliation.affiliationIdentifier", 
-            "contributors.affiliation.affiliationIdentifier",
-            "creators.nameIdentifiers.nameIdentifier",
-            "contributors.nameIdentifiers.nameIdentifier"
-        ]
+        if self.ror:
+            ror_fields = [
+                "publisher.publisherIdentifier",
+                "creators.affiliation.affiliationIdentifier", 
+                "contributors.affiliation.affiliationIdentifier",
+                "creators.nameIdentifiers.nameIdentifier",
+                "contributors.nameIdentifiers.nameIdentifier"
+            ]
+            query_parts.extend([f'{field}:"{self.ror}"' for field in ror_fields])
+        
+        return " OR ".join(query_parts)
 
-        query_parts.extend([f'{field}:"{ror}"' for field in ror_fields])
-    
-    return " OR ".join(query_parts)
+    def api_request_url(self, page_size: int = None) -> str:
+        if page_size is None:
+            page_size = self.page_size
+        params = urllib.parse.urlencode({
+            'page[size]': page_size,
+            'affiliation': 'true',
+            'publisher': 'true',
+            'detail': 'true',
+            'query': self.get_query_string()
+        })
+        return f"https://api.datacite.org/dois?{params}"
 
-def api_request_url(query: str, page_size: int = 100) -> str:    
-    params = urllib.parse.urlencode({
-        'page[size]': page_size,
-        'affiliation': 'true',
-        'publisher': 'true',
-        'query': query
-    })
-    return f"https://api.datacite.org/dois?{params}"
+    @staticmethod
+    def get_api_result(url: str) -> dict:
+        try:
+            with urllib.request.urlopen(url) as response:
+                return json.loads(response.read())
+        except (urllib.error.URLError, json.JSONDecodeError, KeyError) as e:
+            raise RuntimeError(f"Failed run DataCite query: {e}")
 
-def get_api_result(url: str) -> dict:
-    try:
-        with urllib.request.urlopen(url) as response:
-            return json.loads(response.read())
-      
-    except (urllib.error.URLError, json.JSONDecodeError, KeyError) as e:
-        raise RuntimeError(f"Failed run DataCite query: {e}")
-    
-def api_all(query: str) -> list:
-    result = []
-    page_size = 500
-    url = api_request_url(query, page_size)
-    while True:
-        response = get_api_result(url)
-        result.extend(response["data"])
-        logging.info(f"Retrieved {len(result)} of {response['meta']['total']}")
-        if response['links'].get('next'):
-            url = response['links']['next']
-        else:
-            break
-    
-    return result
+    def all(self) -> list:
+        result = []
+        url = self.api_request_url()
+        while True:
+            response = self.get_api_result(url)
+            result.extend(response["data"])
+            logging.info(f"Retrieved {len(result)} of {response['meta']['total']}")
+            if response['links'].get('next'):
+                url = response['links']['next']
+            else:
+                break
+        return result
 
-def datacite_query_result_count(query: str) -> int:
-    if not query:
-        return 0
-    url = api_request_url(query, 0)
-    return get_api_result(url)["meta"]["total"]
+    def count(self) -> int:
+        if not self.get_query_string():
+            return 0
+        url = self.api_request_url(page_size=0)
+        return self.get_api_result(url)["meta"]["total"]
